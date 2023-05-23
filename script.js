f399beaa5e9ebd7e6d45581bdaf655d94d24ccdb
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-23 17:07:10 +0200

    structured header and added style to gridItems

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const gridContainer = document.getElementById('gridContainer');
+const gridContainer = document.getElementById('gridContainer')
 
-function createDivElement(tagname,classname,content); {
 
-    let divElement = document.createElement(tagname);
 
-    divElement.append(content);
 
-    divElement.classList.add(classname);
+function createDivElement(tagname,classname,content) {
 
-    return divElement;
+    let divElement = document.createElement(tagname)
+
+    divElement.append(content)
+
+    divElement.classList.add(classname)
+
+    return divElement
+}
+
+for (let i = 1; i < 101; i++ ) {
+
+    let gridItem = createDivElement('div', 'gridItem', i)
+
+    gridContainer.append(gridItem)
+
+
 }
